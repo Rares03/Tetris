8ca993a3c7e8f648729651b9e1808d042dd58bca
Pop Rareș Andrei <dj.rares69@yahoo.com>
2024-03-11 18:05:06 +0200

    intial commit

---
+++ src/main/java/com/example/tetris/Controller.java
@@ -0,0 +1,437 @@
+package com.example.tetris;
+
+import javafx.scene.shape.Rectangle;
+
+public class Controller extends Shape {
+    public static final int MOVE = Tetris.MOVE;
+    public static final int SIZE = Tetris.SIZE;
+    public static int XMAX = Tetris.XMAX;
+    public static int YMAX = Tetris.YMAX;
+    public static int[][] MESH = Tetris.MESH;
+
+    public Controller(Rectangle a, Rectangle b, Rectangle c, Rectangle d, String name) {
+        super(a, b, c, d, name);
+    }
+
+    public static Shape makeRect() {  //Create shape
+        int block = (int) (Math.random() * 105); //Random shape select
+        String name;
+        Rectangle a = new Rectangle(SIZE-1, SIZE-1),
+                b = new Rectangle(SIZE-1, SIZE-1),
+                c = new Rectangle(SIZE-1, SIZE-1),
+                d = new Rectangle(SIZE-1, SIZE-1);
+        if (block < 15) {
+            a.setX(XMAX / 2 - SIZE);
+            b.setX(XMAX / 2 - SIZE);
+            b.setY(SIZE);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 + SIZE);
+            d.setY(SIZE);
+            name = "j";
+        } else if (block < 30) {
+            a.setX(XMAX / 2 + SIZE);
+            b.setX(XMAX / 2 - SIZE);
+            b.setY(SIZE);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 + SIZE);
+            d.setY(SIZE);
+            name = "l";
+        } else if (block < 45) {
+            a.setX(XMAX / 2 - SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2 - SIZE);
+            c.setY(SIZE);
+            d.setX(XMAX / 2);
+            d.setY(SIZE);
+            name = "o";
+        } else if (block < 60) {
+            a.setX(XMAX / 2 + SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 - SIZE);
+            d.setY(SIZE);
+            name = "s";
+        } else if (block < 75) {
+            a.setX(XMAX / 2 - SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 + SIZE);
+            name = "t";
+        } else if (block < 90) {
+            a.setX(XMAX / 2 + SIZE);
+            b.setX(XMAX / 2);
+            c.setX(XMAX / 2 + SIZE);
+            c.setY(SIZE);
+            d.setX(XMAX / 2 + SIZE + SIZE);
+            d.setY(SIZE);
+            name = "z";
+        } else {
+            a.setX(XMAX / 2 - SIZE - SIZE);
+            b.setX(XMAX / 2 - SIZE);
+            c.setX(XMAX / 2);
+            d.setX(XMAX / 2 + SIZE);
+            name = "i";
+        }
+        return new Shape(a, b, c, d, name);
+    }
+
+    public static void MoveRight(Shape form) {
+        if (form.a.getX() + MOVE <= XMAX - SIZE && form.b.getX() + MOVE <= XMAX - SIZE
+                && form.c.getX() + MOVE <= XMAX - SIZE && form.d.getX() + MOVE <= XMAX - SIZE) {
+            int movea = MESH[((int) form.a.getX() / SIZE) + 1][((int) form.a.getY() / SIZE)];
+            int moveb = MESH[((int) form.b.getX() / SIZE) + 1][((int) form.b.getY() / SIZE)];
+            int movec = MESH[((int) form.c.getX() / SIZE) + 1][((int) form.c.getY() / SIZE)];
+            int moved = MESH[((int) form.d.getX() / SIZE) + 1][((int) form.d.getY() / SIZE)];
+            if (movea == 0 && movea == moveb && moveb == movec && movec == moved) {
+                form.a.setX(form.a.getX() + MOVE);
+                form.b.setX(form.b.getX() + MOVE);
+                form.c.setX(form.c.getX() + MOVE);
+                form.d.setX(form.d.getX() + MOVE);
+            }
+        }
+    }
+
+    public static void MoveLeft(Shape form) {
+        if (form.a.getX() - MOVE >= 0 && form.b.getX() - MOVE >= 0 && form.c.getX() - MOVE >= 0
+                && form.d.getX() - MOVE >= 0) {
+            int movea = MESH[((int) form.a.getX() / SIZE) - 1][((int) form.a.getY() / SIZE)];
+            int moveb = MESH[((int) form.b.getX() / SIZE) - 1][((int) form.b.getY() / SIZE)];
+            int movec = MESH[((int) form.c.getX() / SIZE) - 1][((int) form.c.getY() / SIZE)];
+            int moved = MESH[((int) form.d.getX() / SIZE) - 1][((int) form.d.getY() / SIZE)];
+            if (movea == 0 && movea == moveb && moveb == movec && movec == moved) {
+                form.a.setX(form.a.getX() - MOVE);
+                form.b.setX(form.b.getX() - MOVE);
+                form.c.setX(form.c.getX() - MOVE);
+                form.d.setX(form.d.getX() - MOVE);
+            }
+        }
+    }
+
+    public static void rotate(Shape form) {  //All rotation cases for all shapes
+        int f = form.form;
+        Rectangle a = form.a;
+        Rectangle b = form.b;
+        Rectangle c = form.c;
+        Rectangle d = form.d;
+        switch (form.getName()) {
+            case "j":
+                if (f == 1 && check(a, 1, -1) && check(c, -1, -1) && check(d, -2, -2)) {
+                    MoveRight(form.a);
+                    MoveDown(form.a);
+                    MoveDown(form.c);
+                    MoveLeft(form.c);
+                    MoveDown(form.d);
+                    MoveDown(form.d);
+                    MoveLeft(form.d);
+                    MoveLeft(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 2 && check(a, -1, -1) && check(c, -1, 1) && check(d, -2, 2)) {
+                    MoveDown(form.a);
+                    MoveLeft(form.a);
+                    MoveLeft(form.c);
+                    MoveUp(form.c);
+                    MoveLeft(form.d);
+                    MoveLeft(form.d);
+                    MoveUp(form.d);
+                    MoveUp(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 3 && check(a, -1, 1) && check(c, 1, 1) && check(d, 2, 2)) {
+                    MoveLeft(form.a);
+                    MoveUp(form.a);
+                    MoveUp(form.c);
+                    MoveRight(form.c);
+                    MoveUp(form.d);
+                    MoveUp(form.d);
+                    MoveRight(form.d);
+                    MoveRight(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 4 && check(a, 1, 1) && check(c, 1, -1) && check(d, 2, -2)) {
+                    MoveUp(form.a);
+                    MoveRight(form.a);
+                    MoveRight(form.c);
+                    MoveDown(form.c);
+                    MoveRight(form.d);
+                    MoveRight(form.d);
+                    MoveDown(form.d);
+                    MoveDown(form.d);
+                    form.setRotation();
+                    break;
+                }
+                break;
+            case "l":
+                if (f == 1 && check(a, 1, -1) && check(c, 1, 1) && check(b, 2, 2)) {
+                    MoveRight(form.a);
+                    MoveDown(form.a);
+                    MoveUp(form.c);
+                    MoveRight(form.c);
+                    MoveUp(form.b);
+                    MoveUp(form.b);
+                    MoveRight(form.b);
+                    MoveRight(form.b);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 2 && check(a, -1, -1) && check(b, 2, -2) && check(c, 1, -1)) {
+                    MoveDown(form.a);
+                    MoveLeft(form.a);
+                    MoveRight(form.b);
+                    MoveRight(form.b);
+                    MoveDown(form.b);
+                    MoveDown(form.b);
+                    MoveRight(form.c);
+                    MoveDown(form.c);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 3 && check(a, -1, 1) && check(c, -1, -1) && check(b, -2, -2)) {
+                    MoveLeft(form.a);
+                    MoveUp(form.a);
+                    MoveDown(form.c);
+                    MoveLeft(form.c);
+                    MoveDown(form.b);
+                    MoveDown(form.b);
+                    MoveLeft(form.b);
+                    MoveLeft(form.b);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 4 && check(a, 1, 1) && check(b, -2, 2) && check(c, -1, 1)) {
+                    MoveUp(form.a);
+                    MoveRight(form.a);
+                    MoveLeft(form.b);
+                    MoveLeft(form.b);
+                    MoveUp(form.b);
+                    MoveUp(form.b);
+                    MoveLeft(form.c);
+                    MoveUp(form.c);
+                    form.setRotation();
+                    break;
+                }
+                break;
+            case "o":
+                break;
+            case "s":
+                if (f == 1 && check(a, -1, -1) && check(c, -1, 1) && check(d, 0, 2)) {
+                    MoveDown(form.a);
+                    MoveLeft(form.a);
+                    MoveLeft(form.c);
+                    MoveUp(form.c);
+                    MoveUp(form.d);
+                    MoveUp(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 2 && check(a, 1, 1) && check(c, 1, -1) && check(d, 0, -2)) {
+                    MoveUp(form.a);
+                    MoveRight(form.a);
+                    MoveRight(form.c);
+                    MoveDown(form.c);
+                    MoveDown(form.d);
+                    MoveDown(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 3 && check(a, -1, -1) && check(c, -1, 1) && check(d, 0, 2)) {
+                    MoveDown(form.a);
+                    MoveLeft(form.a);
+                    MoveLeft(form.c);
+                    MoveUp(form.c);
+                    MoveUp(form.d);
+                    MoveUp(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 4 && check(a, 1, 1) && check(c, 1, -1) && check(d, 0, -2)) {
+                    MoveUp(form.a);
+                    MoveRight(form.a);
+                    MoveRight(form.c);
+                    MoveDown(form.c);
+                    MoveDown(form.d);
+                    MoveDown(form.d);
+                    form.setRotation();
+                    break;
+                }
+                break;
+            case "t":
+                if (f == 1 && check(a, 1, 1) && check(d, -1, -1) && check(c, -1, 1)) {
+                    MoveUp(form.a);
+                    MoveRight(form.a);
+                    MoveDown(form.d);
+                    MoveLeft(form.d);
+                    MoveLeft(form.c);
+                    MoveUp(form.c);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 2 && check(a, 1, -1) && check(d, -1, 1) && check(c, 1, 1)) {
+                    MoveRight(form.a);
+                    MoveDown(form.a);
+                    MoveLeft(form.d);
+                    MoveUp(form.d);
+                    MoveUp(form.c);
+                    MoveRight(form.c);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 3 && check(a, -1, -1) && check(d, 1, 1) && check(c, 1, -1)) {
+                    MoveDown(form.a);
+                    MoveLeft(form.a);
+                    MoveUp(form.d);
+                    MoveRight(form.d);
+                    MoveRight(form.c);
+                    MoveDown(form.c);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 4 && check(a, -1, 1) && check(d, 1, -1) && check(c, -1, -1)) {
+                    MoveLeft(form.a);
+                    MoveUp(form.a);
+                    MoveRight(form.d);
+                    MoveDown(form.d);
+                    MoveDown(form.c);
+                    MoveLeft(form.c);
+                    form.setRotation();
+                    break;
+                }
+                break;
+            case "z":
+                if (f == 1 && check(b, 1, 1) && check(c, -1, 1) && check(d, -2, 0)) {
+                    MoveUp(form.b);
+                    MoveRight(form.b);
+                    MoveLeft(form.c);
+                    MoveUp(form.c);
+                    MoveLeft(form.d);
+                    MoveLeft(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 2 && check(b, -1, -1) && check(c, 1, -1) && check(d, 2, 0)) {
+                    MoveDown(form.b);
+                    MoveLeft(form.b);
+                    MoveRight(form.c);
+                    MoveDown(form.c);
+                    MoveRight(form.d);
+                    MoveRight(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 3 && check(b, 1, 1) && check(c, -1, 1) && check(d, -2, 0)) {
+                    MoveUp(form.b);
+                    MoveRight(form.b);
+                    MoveLeft(form.c);
+                    MoveUp(form.c);
+                    MoveLeft(form.d);
+                    MoveLeft(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 4 && check(b, -1, -1) && check(c, 1, -1) && check(d, 2, 0)) {
+                    MoveDown(form.b);
+                    MoveLeft(form.b);
+                    MoveRight(form.c);
+                    MoveDown(form.c);
+                    MoveRight(form.d);
+                    MoveRight(form.d);
+                    form.setRotation();
+                    break;
+                }
+                break;
+            case "i":
+                if (f == 1 && check(a, 2, 2) && check(b, 1, 1) && check(d, -1, -1)) {
+                    MoveUp(form.a);
+                    MoveUp(form.a);
+                    MoveRight(form.a);
+                    MoveRight(form.a);
+                    MoveUp(form.b);
+                    MoveRight(form.b);
+                    MoveDown(form.d);
+                    MoveLeft(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 2 && check(a, -2, -2) && check(b, -1, -1) && check(d, 1, 1)) {
+                    MoveDown(form.a);
+                    MoveDown(form.a);
+                    MoveLeft(form.a);
+                    MoveLeft(form.a);
+                    MoveDown(form.b);
+                    MoveLeft(form.b);
+                    MoveUp(form.d);
+                    MoveRight(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 3 && check(a, 2, 2) && check(b, 1, 1) && check(d, -1, -1)) {
+                    MoveUp(form.a);
+                    MoveUp(form.a);
+                    MoveRight(form.a);
+                    MoveRight(form.a);
+                    MoveUp(form.b);
+                    MoveRight(form.b);
+                    MoveDown(form.d);
+                    MoveLeft(form.d);
+                    form.setRotation();
+                    break;
+                }
+                if (f == 4 && check(a, -2, -2) && check(b, -1, -1) && check(d, 1, 1)) {
+                    MoveDown(form.a);
+                    MoveDown(form.a);
+                    MoveLeft(form.a);
+                    MoveLeft(form.a);
+                    MoveDown(form.b);
+                    MoveLeft(form.b);
+                    MoveUp(form.d);
+                    MoveRight(form.d);
+                    form.setRotation();
+                    break;
+                }
+                break;
+        }
+    }
+
+    private static void MoveDown(Rectangle rect) {
+        if (rect.getY() + MOVE < YMAX)
+            rect.setY(rect.getY() + MOVE);
+
+    }
+
+    private static void MoveRight(Rectangle rect) {
+        if (rect.getX() + MOVE <= XMAX - SIZE)
+            rect.setX(rect.getX() + MOVE);
+    }
+
+    private static void MoveLeft(Rectangle rect) {
+        if (rect.getX() - MOVE >= 0)
+            rect.setX(rect.getX() - MOVE);
+    }
+
+    private static void MoveUp(Rectangle rect) {
+        if (rect.getY() - MOVE > 0)
+            rect.setY(rect.getY() - MOVE);
+    }
+
+    private static boolean check(Rectangle rect, int x, int y) { //Check if piece can be rotated
+        boolean xb = false;
+        boolean yb = false;
+        if (x >= 0)
+            xb = rect.getX() + x * MOVE <= XMAX - SIZE;
+        if (x < 0)
+            xb = rect.getX() + x * MOVE >= 0;
+        if (y >= 0)
+            yb = rect.getY() - y * MOVE > 0;
+        if (y < 0)
+            yb = rect.getY() + y * MOVE < YMAX;
+        return xb && yb && MESH[((int) rect.getX() / SIZE) + x][((int) rect.getY() / SIZE) - y] == 0;
+    }
+
+}
---
+++ src/main/java/com/example/tetris/Tetris.java
@@ -0,0 +1,254 @@
+package com.example.tetris;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Tetris extends Application {
+    public static final int MOVE = 25; //distance we move the rectangles
+    public static final int SIZE = 25;
+    public static int XMAX = SIZE * 12;
+    public static int YMAX = SIZE * 24;
+    public static int[][] MESH = new int[XMAX / SIZE][YMAX / SIZE];
+    private static Pane group = new Pane();
+    private static Shape object;
+    private static Scene scene = new Scene(group, XMAX + 150, YMAX);
+    public static int score = 0;
+    private static int top = 0;
+    private static boolean game = true;
+    private static Shape nextObj = Controller.makeRect();
+    private static int linesNr = 0;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {   // Start app
+        for (int[] a : MESH) {
+            Arrays.fill(a, 0);
+        }
+        Image image = null;
+        try {
+            image = new Image(Tetris.class.getResourceAsStream("/background_image.png"));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        BackgroundImage backgroundImage =
+                new BackgroundImage(
+                        image,
+                        BackgroundRepeat.NO_REPEAT,  // repeat X
+                        BackgroundRepeat.NO_REPEAT,  // repeat Y
+                        BackgroundPosition.DEFAULT,   // position
+                        new BackgroundSize(
+                                100,   // width  = 100%
+                                100,   // height = 100%
+                                true,  // width is percentage
+                                true,  // height is percentage
+                                true,  // contain image within bounds
+                                true   // cover all of Region content area
+                        )
+                );
+
+        group.setBackground(new Background(backgroundImage));
+
+        Line line = new Line(XMAX, 0, XMAX, YMAX);
+        Text scoretext = new Text("Score: ");
+        scoretext.setStyle("-fx-font: 20 arial;");
+        scoretext.setY(50);
+        scoretext.setX(XMAX + 5);
+        Text level = new Text("Lines: ");
+        level.setStyle("-fx-font: 20 arial;");
+        level.setY(100);
+        level.setX(XMAX + 5);
+        group.getChildren().addAll(scoretext, line, level);
+
+        Shape a = nextObj;
+        group.getChildren().addAll(a.a, a.b, a.c, a.d);
+        moveOnKeyPress(a);
+        object = a;
+        nextObj = Controller.makeRect();
+        stage.setScene(scene);
+        stage.setTitle("TETRIS");
+        stage.show();
+
+        Timer fall = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    public void run() {
+                        if (object.a.getY() == 0 || object.b.getY() == 0 || object.c.getY() == 0
+                                || object.d.getY() == 0)
+                            top++;
+                        else
+                            top = 0;
+
+                        if (top == 2) {
+                            // GAME OVER
+                            Text over = new Text("GAME OVER");
+                            over.setFill(Color.RED);
+                            over.setStyle("-fx-font: 70 arial;");
+                            over.setY(250);
+                            over.setX(10);
+                            group.getChildren().add(over);
+                            game = false;
+                        }
+                        // Exit
+                        if (top == 15) {
+                            System.exit(0);
+                        }
+
+                        if (game) {
+                            MoveDown(object);
+                            scoretext.setText("Score: " + Integer.toString(score));
+                            level.setText("Lines: " + Integer.toString(linesNr));
+                        }
+                    }
+                });
+            }
+        };
+        fall.schedule(task, 0, 300);
+    }
+
+    private void moveOnKeyPress(Shape form) {     //Handle user input
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case RIGHT:
+                        Controller.MoveRight(form);
+                        break;
+                    case DOWN:
+                        MoveDown(form);
+                        break;
+                    case LEFT:
+                        Controller.MoveLeft(form);
+                        break;
+                    case UP:
+                        Controller.rotate(form);
+                        break;
+                }
+            }
+        });
+    }
+    private void RemoveRows(Pane pane) {  //Check if there are any completed lines and if yes delete them
+        ArrayList<Node> rects = new ArrayList<Node>();
+        ArrayList<Integer> lines = new ArrayList<Integer>();
+        ArrayList<Node> newrects = new ArrayList<Node>();
+        int full = 0;
+        for (int i = 0; i < MESH[0].length; i++) {
+            for (int j = 0; j < MESH.length; j++) {
+                if (MESH[j][i] == 1)
+                    full++;
+            }
+            if (full == MESH.length)
+                lines.add(i);
+            full = 0;
+        }
+        if (lines.size() > 0) {
+            int multiplier=0;
+            do {
+                for (Node node : pane.getChildren()) {
+                    if (node instanceof Rectangle)
+                        rects.add(node);
+                }
+                linesNr++;
+                multiplier++;
+                for (Node node : rects) {
+                    Rectangle a = (Rectangle) node;
+                    if (a.getY() == lines.get(0) * SIZE) {
+                        MESH[(int) a.getX() / SIZE][(int) a.getY() / SIZE] = 0;
+                        pane.getChildren().remove(node);
+                    } else
+                        newrects.add(node);
+                }
+
+                for (Node node : newrects) {
+                    Rectangle a = (Rectangle) node;
+                    if (a.getY() < lines.get(0) * SIZE) {
+                        MESH[(int) a.getX() / SIZE][(int) a.getY() / SIZE] = 0;
+                        a.setY(a.getY() + SIZE);
+                    }
+                }
+                lines.remove(0);
+                rects.clear();
+                newrects.clear();
+                for (Node node : pane.getChildren()) {
+                    if (node instanceof Rectangle)
+                        rects.add(node);
+                }
+                for (Node node : rects) {
+                    Rectangle a = (Rectangle) node;
+                    try {
+                        MESH[(int) a.getX() / SIZE][(int) a.getY() / SIZE] = 1;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                    }
+                }
+                rects.clear();
+            } while (!lines.isEmpty());
+            score += multiplier*(50*multiplier);
+        }
+    }
+
+    private void MoveDown(Shape form) { //Move shape down
+        if (form.a.getY() == YMAX - SIZE || form.b.getY() == YMAX - SIZE || form.c.getY() == YMAX - SIZE
+                || form.d.getY() == YMAX - SIZE || moveA(form) || moveB(form) || moveC(form) || moveD(form)) { //if piece is donw
+            MESH[(int) form.a.getX() / SIZE][(int) form.a.getY() / SIZE] = 1;
+            MESH[(int) form.b.getX() / SIZE][(int) form.b.getY() / SIZE] = 1;
+            MESH[(int) form.c.getX() / SIZE][(int) form.c.getY() / SIZE] = 1;
+            MESH[(int) form.d.getX() / SIZE][(int) form.d.getY() / SIZE] = 1;
+            RemoveRows(group);
+
+            Shape a = nextObj;  //create next shape
+            nextObj = Controller.makeRect();
+            object = a;
+            group.getChildren().addAll(a.a, a.b, a.c, a.d);
+            moveOnKeyPress(a);
+        }
+
+        if (form.a.getY() + MOVE < YMAX && form.b.getY() + MOVE < YMAX && form.c.getY() + MOVE < YMAX
+                && form.d.getY() + MOVE < YMAX) {  //if piece can still move
+            int movea = MESH[(int) form.a.getX() / SIZE][((int) form.a.getY() / SIZE) + 1];
+            int moveb = MESH[(int) form.b.getX() / SIZE][((int) form.b.getY() / SIZE) + 1];
+            int movec = MESH[(int) form.c.getX() / SIZE][((int) form.c.getY() / SIZE) + 1];
+            int moved = MESH[(int) form.d.getX() / SIZE][((int) form.d.getY() / SIZE) + 1];
+            if (movea == 0 && movea == moveb && moveb == movec && movec == moved) {
+                form.a.setY(form.a.getY() + MOVE);
+                form.b.setY(form.b.getY() + MOVE);
+                form.c.setY(form.c.getY() + MOVE);
+                form.d.setY(form.d.getY() + MOVE);
+            }
+        }
+    }
+
+    private boolean moveA(Shape form) {  //check if rectangle a can be moved
+        return (MESH[(int) form.a.getX() / SIZE][((int) form.a.getY() / SIZE) + 1] == 1);
+    }
+
+    private boolean moveB(Shape form) {  //check if rectangle b can be moved
+        return (MESH[(int) form.b.getX() / SIZE][((int) form.b.getY() / SIZE) + 1] == 1);
+    }
+
+    private boolean moveC(Shape form) {  //check if rectangle c can be moved
+        return (MESH[(int) form.c.getX() / SIZE][((int) form.c.getY() / SIZE) + 1] == 1);
+    }
+
+    private boolean moveD(Shape form) {  //check if rectangle d can be moved
+        return (MESH[(int) form.d.getX() / SIZE][((int) form.d.getY() / SIZE) + 1] == 1);
+    }
+}
